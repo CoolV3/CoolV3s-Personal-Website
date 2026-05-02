055f92b9d51acf588dff2a23a5c14b135e236752
Constantin Meier <meierconsti@web.de>
2026-05-02 11:25:15 +0200

    Made the Project automatically decide how many objects to load based on screen size.

--- src/components/myProjekts.tsx
+++ src/components/myProjekts.tsx
@@ -3,12 +3,33 @@
 import Image from "next/image";
 import projectsData from "../app/data/myProjects.json"
 import Link from "next/link";
-import {useState} from "react";
+import {useState, useEffect} from "react";
 
 
 export default function ProjektSection({ howMany }: { howMany?: number}) {
     const [projectCount, setProjectCount] = useState(howMany)
+    const [expanded, setExpanded] = useState(false);
 
+    useEffect(() => {
+        const showProjects = () => {
+            if (expanded) return
+
+            if (window.innerWidth < 640) {
+                setProjectCount(2)
+            } else if (window.innerWidth < 768) {
+                setProjectCount(4)
+            } else if (window.innerWidth < 1024) {
+                setProjectCount(6)
+            } else {
+                setProjectCount(8)
+            }
+        }
+
+        showProjects();
+        window.addEventListener("resize", showProjects)
+
+        return () => window.removeEventListener("resize", showProjects)
+    }, [expanded])
 
     const Projects = projectCount != null ? projectsData.Projects.slice(0, projectCount) : projectsData.Projects
 
@@ -31,7 +52,14 @@ export default function ProjektSection({ howMany }: { howMany?: number}) {
             </div>
             { projectCount != null && projectCount < projectsData.Projects.length &&  (
             <div className="pt-10">
-                <button className="text-white bg-black px-15 py-5 rounded-2xl cursor-pointer hover:bg-amber-500 hover:text-black transition-colors duration-400" onClick={(e) => setProjectCount(projectCount + 3)}>Load More</button>
+                <button className="text-white bg-black px-15 py-5 rounded-2xl cursor-pointer hover:bg-amber-500 hover:text-black transition-colors duration-400"
+                        onClick={() => {
+                    setExpanded(true);
+                    if (projectCount != null) {
+                        setProjectCount(projectCount + 3);
+                    }
+                }}
+                >Load More</button>
             </div>
                 )}
         </div>
